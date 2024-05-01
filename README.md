698158b42e3ef4b1ceb3d7c4ab8a1c65a70a0dc4
Alexandre Beato <alebeato9@gmail.com>
2024-05-01 12:36:52 -0300

    Added try and catch

--- README.md
+++ README.md
@@ -2,20 +2,26 @@
 
 This is a simple script to automate the speed test using the speedtest-cli library. Download the executable file from [releases](https://github.com/alexandrebeato/speedtest/releases) and run it. The script will run the speed test and save the results in a CSV file in the same directory.
 
-## Building the executable
+## Running or building the executable
 
 First of all, create an virtual environment and install the dependencies:
 
 ```bash
-python -m venv speedtest
+py -m venv speedtest
 source speedtest/bin/activate
 pip install -r requirements.txt
+```
+
+To run the script, just execute the `main.py` file:
+
+```bash
+py main.py
 ```
 
 Then, build the executable:
 
 ```bash
-python -m PyInstaller --onefile main.py
+py -m PyInstaller --onefile main.py
 ```
 
 The executable will be in the `dist` directory.
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tabulate import tabulate
 import time
 from pandas import DataFrame
 import os
+
 clear = lambda: os.system('cls')
 
 table_headers = ["Datetime", "Server ID", "Server Name", "Latency", "Download Mbps", "Upload Mbps"]
@@ -19,33 +20,38 @@ while True:
     
     is_loading = True
     
-    st = speedtest.Speedtest(secure=True)
+    try:
+        st = speedtest.Speedtest(secure=True)
 
-    # Get the best server based on ping
-    st.get_best_server()
+        # Get the best server based on ping
+        st.get_best_server()
 
-    # Perform download and upload speed tests
-    download_speed = st.download()
-    upload_speed = st.upload()
+        # Perform download and upload speed tests
+        download_speed = st.download()
+        upload_speed = st.upload()
 
-    # Convert speeds to Mbps
-    date_time = time.strftime("%Y-%m-%d %H:%M:%S")
-    server_name = st.results.server["name"]
-    server_id = st.results.server["id"]
-    latency = st.results.ping
-    download_speed_mbps = round(download_speed / 1_000_000, 2)
-    upload_speed_mbps = round(upload_speed / 1_000_000, 2)
+        # Convert speeds to Mbps
+        date_time = time.strftime("%Y-%m-%d %H:%M:%S")
+        server_name = st.results.server["name"]
+        server_id = st.results.server["id"]
+        latency = st.results.ping
+        download_speed_mbps = round(download_speed / 1_000_000, 2)
+        upload_speed_mbps = round(upload_speed / 1_000_000, 2)
+        
+        table_data.append([date_time, server_id, server_name, latency, download_speed_mbps, upload_speed_mbps])
+        
+        is_loading = False
+        
+        # Save the data to a CSV file
+        df = DataFrame(table_data, columns=table_headers)
+        df.to_csv("speedtest.csv", index=False)
+        
+        clear()
+        
+        print(tabulate(table_data, headers=table_headers, tablefmt="pretty"))
+        
+        time.sleep(60)
     
-    table_data.append([date_time, server_id, server_name, latency, download_speed_mbps, upload_speed_mbps])
-    
-    is_loading = False
-    
-    # Save the data to a CSV file
-    df = DataFrame(table_data, columns=table_headers)
-    df.to_csv("speedtest.csv", index=False)
-    
-    clear()
-    
-    print(tabulate(table_data, headers=table_headers, tablefmt="pretty"))
-    
-    time.sleep(60)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        is_loading = False
